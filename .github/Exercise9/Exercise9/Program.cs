d9075b7d6a6ef54f7ca5d6a1f0afefa3da65bf8a
Elijah Williams <145467298+ForTheLoveOfGoat-EW@users.noreply.github.com>
2024-03-06 09:52:08 -0500

    done with 9

--- .github/Exercise9/Exercise9/Program.cs
+++ .github/Exercise9/Exercise9/Program.cs
@@ -15,12 +15,25 @@
             Console.WriteLine();
             for (int i = 0; i < rows; i++)
             {
-                for (int j = 0; j < with; j++) 
+                for (int j = 0; j < with; j++)
                 {
                     Console.Write("*");
                 }
-                Console.WriteLine();
+                Console.WriteLine("");
+            }
+
+            // convert strings to int and read line
+            Console.WriteLine("Enter the maximum amount of Asterisks in a row ");
+            int MaxRow = Int32.Parse(Console.ReadLine());
+            //print rows
+            for (int i = 1; i <= MaxRow; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
             }
         }
     }
-} 
+}
