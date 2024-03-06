56cba3c56fd38d0cecea47c9f536500ee33328e3
Elijah Williams <145467298+ForTheLoveOfGoat-EW@users.noreply.github.com>
2024-03-06 09:29:36 -0500

    done with Exercise 9

---
+++ .github/Exercise9/Exercise9/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercise9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // convert strings to int and read line
+            Console.WriteLine("Enter row with ");
+            int with = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many rows? ");
+            int rows = Int32.Parse(Console.ReadLine());
+
+            //print rows
+            Console.WriteLine();
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < with; j++) 
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+} 
